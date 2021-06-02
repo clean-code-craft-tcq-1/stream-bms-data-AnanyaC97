942622b136a39332826aec4a3254f301c40df670
Ananya Chandramohan <77439415+AnanyaC97@users.noreply.github.com>
2021-06-02 17:02:39 +0530

    Update Program.cs

--- Sender/BatteryStreamer/Program.cs
+++ Sender/BatteryStreamer/Program.cs
@@ -1,27 +1,33 @@
-﻿using System;
+using System;
 
 namespace BatteryStreamer
 {
     public class Program
     {
+        public static int readingCount = 0;
         public static void Main(string[] args)
         {
             Console.WriteLine("Enter the number of readings to be considered - Minimum 15");
-            int readingCount = int.Parse(Console.ReadLine());
+            readingCount = int.Parse(Console.ReadLine());
 
             if (BatteryStreamData.IsReadingCountGreaterThan15(readingCount))
             {
-                BatteryStreamData batteryStreamData = new BatteryStreamData();
-                for(int count = 0; count < readingCount; count++)
-                {
-                    batteryStreamData.GenerateBatteryReadings();
-                    Console.WriteLine("Temperature: " + batteryStreamData.BatteryReadings[count].Temperature + " Charge Rate: " + batteryStreamData.BatteryReadings[count].ChargeRate);
-                    System.Threading.Thread.Sleep(5000);
-                }
+                StreamBatteryReadings();
             }
             else
             {
                 Console.WriteLine("Minimum readings value should be 15");
+            }
+        }
+
+        public static void StreamBatteryReadings()
+        {
+            BatteryStreamData batteryStreamData = new BatteryStreamData();
+            for (int count = 0; count < readingCount; count++)
+            {
+                batteryStreamData.GenerateBatteryReadings();
+                Console.WriteLine("Temperature: " + batteryStreamData.BatteryReadings[count].Temperature + " Charge Rate: " + batteryStreamData.BatteryReadings[count].ChargeRate);
+                System.Threading.Thread.Sleep(5000);
             }
         }
     }
